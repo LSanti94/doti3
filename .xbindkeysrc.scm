060dc4149e52048d3dcfddbff1d74132877e090c
Reza Shams Amiri <rezasa@axis.com>
2018-05-16 22:39:06 +0200

    updated configuration

--- .xbindkeysrc.scm
+++ .xbindkeysrc.scm
@@ -11,20 +11,25 @@
 ;;
 ;; It also allow to add or remove a key on the fly!
 
-(xbindkey '(control shift q) "xbindkeys_show")
+;; use xbindkeys-config to get the correct key-configuration
 
+;; Move pointer to position x,y
 (xbindkey '(Menu) "xdotool mousemove 640 800; xdotool mousemove 960 0")
 
+;; Ctrl+ Right Click should play sound
 (xbindkey '(control "b:3") "pn mclick")
 
+;; Shift+Click simulated middle click and plays a sound
 (xbindkey '(release "m:0x3" "b:1" ) "/usr/bin/xte 'mouseclick 2';pn mclick;")
 
+;; Shift+Wheel Up/Down should run fast scroll+sounds 
 (xbindkey '("m:0x11" "b:5") "~/bini3/i3-mousewheel up")
 (xbindkey '("m:0x11" "b:4") "~/bini3/i3-mousewheel down")
 
 ; (xbindkey '(release shift "b:5") "pn mclick;xte 'mousedown 5' 'mouseclick 5' 'mouseup 5'")
 ; (xbindkey '(shift "b:4") "repeat 3|xte 'mouseclick 4'")
 
+;; Shift + PageDown/Pageup should result in Home/End
 (xbindkey '(release "m:0x3" "Prior") "xdotool key --clearmodifiers Home")
 (xbindkey '(release "m:0x3" "Next")  "xdotool key --clearmodifiers End")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
---
+++ install/install.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+echo "Installing prerequisites for this configuration:\n"
+
+# *************************************************************
+# ************ xbindkeys and related utilities ****************
+# *************************************************************
+
+# this will include xte which is used in xbindkeys configuration
+sudo apt install xautomation 
+# this will install lockfile-remove etc, which is used in `pn` script for playing sounds
+# or in i3-mousewheel for fast scrolling
+sudo apt install lockfile-progs
+# xdotool is used in some bindingings of xbindkeys
+sudo apt install xdotool
+# xbindkeys for additional shortcut configuration
+sudo apt install xbindkeys xbindkeys_config
+
+
+# For mounting remote file systems
+sudo apt install nfs-common cifs-utils
