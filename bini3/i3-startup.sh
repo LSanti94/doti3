7659f646a51302a479e8a7779a6e781145928c20
Reza Shams Amiri <rezasa@axis.com>
2018-06-11 23:50:35 +0200

    organized i3-startup

--- bini3/i3-startup.sh
+++ bini3/i3-startup.sh
@@ -25,7 +25,18 @@ killall nm-applet &> /dev/null
 exec "/usr/bin/nm-applet" &> /dev/null &
 
 # exec "mate-settings-daemon" --replace &> /dev/null & 
+
+
+# to fix redshift error for geolocation edit `/etc/geoclue/geoclue.conf` add:
+# [redshift]
+#   allowed=true
+#   system=false
+#   users=
+# also:
+#  sudo apt-get remove geoclue*
+#  sudo apt-get install geoclue-2.0
 killall redshift &> /dev/null  
+killall redshift-gtk &> /dev/null  
 exec "redshift-gtk" &> /dev/null &
 
 # $HOME/bini3/toggle-gaps set
@@ -33,18 +44,23 @@ exec "redshift-gtk" &> /dev/null &
 killall albert &> /dev/null  
 exec "/usr/bin/albert" &> /dev/null &
 
-killall xbindkeys
+killall xbindkeys &> /dev/null  
 exec "/usr/bin/xbindkeys" &> /dev/null &
 
-(
-   sleep 3 && \
-   $HOME/bini3/,o1 "/usr/bin/google-chrome" --force-device-scale-factor=1.25 &> /dev/null 
-)&
-(
-   sleep 15 && \
-   exec `which nitrogen` --restore &> /dev/null 
-   notify.sh "nitrogen background restored" -i info && \
-)&
+killall compton &> /dev/null
+compton --config ~/.config/compton.conf -b
+#it's important that you distinguish between the command and its parameters
+#Open in        $Workspace $WaitTime   $Command
+$HOME/bini3/,ow 2          0           "/usr/bin/terminator" 
+$HOME/bini3/,ow 2          1           "/usr/bin/thunar" --daemon
+$HOME/bini3/,ow 1          3           "/usr/bin/google-chrome" --force-device-scale-factor=1.25 &
 
-# compton --config ~/.config/compton.conf -b
+exec "$HOME/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox" --minimize &> /dev/null &
+exec `which nitrogen` --restore &> /dev/null &
+
+amixer -D pulse sset Master 60% > /dev/null
+$HOME/bini3/,volume up # to show the current volume on i3bar
+paplay ~/sounds/milton__kualalumpur-international.wav &
+
+
 
