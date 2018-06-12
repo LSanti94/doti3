a6c48f53a737aac8890577ef93a4040c1611f3db
Reza Shams Amiri <rezasa@axis.com>
2018-06-13 01:13:04 +0200

    Switched to the new way of saving/loading layout
    Thanks to this quesiton:
    https://unix.stackexchange.com/questions/134980/how-do-you-implement-layout-with-the-autostarting-of-applications-in-the-i3-wind?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa

--- bini3/i3-startup.sh
+++ bini3/i3-startup.sh
@@ -25,6 +25,9 @@ killall nm-applet &> /dev/null
 exec "/usr/bin/nm-applet" &> /dev/null &
 
 # exec "mate-settings-daemon" --replace &> /dev/null & 
+killall dunst; dunst -config ~/.config/dunst/dunstrc &
+$HOME/bin/flog "dunst loaded"
+
 
 
 # to fix redshift error for geolocation edit `/etc/geoclue/geoclue.conf` add:
@@ -37,7 +40,6 @@ exec "/usr/bin/nm-applet" &> /dev/null &
 #  sudo apt-get install geoclue-2.0
 killall redshift &> /dev/null  
 killall redshift-gtk &> /dev/null  
-exec "redshift-gtk" &> /dev/null &
 
 # $HOME/bini3/toggle-gaps set
 
@@ -51,12 +53,32 @@ killall compton &> /dev/null
 compton --config ~/.config/compton.conf -b
 #it's important that you distinguish between the command and its parameters
 #Open in        $Workspace $WaitTime   $Command
-$HOME/bini3/,ow 2          0           "/usr/bin/terminator" 
-$HOME/bini3/,ow 2          1           "/usr/bin/thunar" --daemon
-$HOME/bini3/,ow 1          3           "/usr/bin/google-chrome" --force-device-scale-factor=1.25 &
+W1=$(xgetres i3.w1)
+W2=$(xgetres i3.w2)
+
+# This is the result of i3-save layout
+#     i3-msg rename workspace to 1
+#     i3-save-tree --workspace 1 > browser.json
+#
+# don't forget that you should edit the file afterwards
+i3-msg "workspace $W1; append_layout $HOME/bini3/layout/w1-browser.json"
+i3-msg "workspace $W2; append_layout $HOME/bini3/layout/w2-terminal.json"
+
+# $HOME/bini3/,ow 2          0           "/usr/bin/terminator" 
+# $HOME/bini3/,ow 2          1           "/usr/bin/thunar" --daemon
+# $HOME/bini3/,ow 1          3           "/usr/bin/google-chrome" --force-device-scale-factor=1.25 &
+# $HOME/bini3/,ow 1          15          "redshift-gtk" &
+
+exec /usr/bin/terminator &
+exec /usr/bin/terminator &
+exec "/usr/bin/thunar" --daemon &
+exec /usr/bin/google-chrome --force-device-scale-factor=1.25 &
+exec gedit ~/notes.md
+exec redshift-gtk &
 
 exec "$HOME/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox" --minimize &> /dev/null &
 exec `which nitrogen` --restore &> /dev/null &
+$HOME/bin/flog "nitrogen restored"
 
 amixer -D pulse sset Master 60% > /dev/null
 $HOME/bini3/,volume up # to show the current volume on i3bar
